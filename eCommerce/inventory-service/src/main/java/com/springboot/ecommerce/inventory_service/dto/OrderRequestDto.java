c7a9b06c7cbe8df7e0d47f5540e7557ed6820697
VishalPatel8181 <vishalkumarpatel.cr@merillife.com>
2025-09-14 19:56:29 +0530

    2 Microservices with discovery service

---
+++ eCommerce/inventory-service/src/main/java/com/springboot/ecommerce/inventory_service/dto/OrderRequestDto.java
@@ -0,0 +1,10 @@
+package com.springboot.ecommerce.inventory_service.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class OrderRequestDto {
+    private List<OrderRequestItemDto> items;
+}
